ce113489152aacab875a834537a0bbea5c4bb538
Luis Combariza <luis_combariza@outlook.com>
2021-02-17 11:41:20 -0700

    updated MessagfeForm file

--- chat-application/src/App.js
+++ chat-application/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <ChatEngine
       height="100vh"
       projectID="1c2d3e91-d82b-4629-b1dc-75dbc8a8a6e9"
-      userName="Luis Combariza"
+      userName="LuisCombariza"
       userSecret="12345"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
