8b667ac5999cd0707bff0c788b1e64d12758e837
Luis Combariza <luis_combariza@outlook.com>
2021-02-17 10:17:04 -0700

    Updated ChatFeed.jsx

--- chat-application/src/components/ChatFeed.jsx
+++ chat-application/src/components/ChatFeed.jsx
@@ -8,7 +8,37 @@ const ChatFeed = (props) => {
   const chat = chats && chats[activeChat];
   console.log(chat, userName, messages);
 
-  return <div>ChatFeed</div>;
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    console.log(keys);
+
+    return keys.map((key, index) => {
+      const messages = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+          </div>
+          <div
+            className="read-receipts"
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          >
+            read-receipts
+          </div>
+        </div>
+      );
+    });
+  };
+
+  renderMessages();
+
+  return <div></div>;
 };
 
 export default ChatFeed;
