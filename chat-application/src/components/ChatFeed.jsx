15dd26c1fda80579695400dc65ac788f3f74946c
Luis Combariza <luis_combariza@outlook.com>
2021-02-17 12:47:56 -0700

    Added file for Login user entry

--- chat-application/src/components/ChatFeed.jsx
+++ chat-application/src/components/ChatFeed.jsx
@@ -8,6 +8,22 @@ const ChatFeed = (props) => {
   const chat = chats && chats[activeChat];
   console.log(chat, userName, messages);
 
+  const renderReadReceipts = (message, isMyMessage) => {
+    return chat.people.map(
+      (person, index) =>
+        person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+              float: isMyMessage ? "right" : "left",
+              blackgroundImage: `url(${person?.person?.avatar})`,
+            }}
+          />
+        )
+    );
+  };
+
   const renderMessages = () => {
     const keys = Object.keys(messages);
     console.log(keys);
