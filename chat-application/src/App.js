fa864492f21ae51c885ac32029dcccdee0c6b86d
Luis Combariza <luis_combariza@outlook.com>
2021-02-17 10:06:49 -0700

    Added component folder and inital code for component objects

--- chat-application/src/App.js
+++ chat-application/src/App.js
@@ -1,6 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="1c2d3e91-d82b-4629-b1dc-75dbc8a8a6e9"
+      userName="Luis Combariza"
+      userSecret="12345"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
 };
+
+export default App;
